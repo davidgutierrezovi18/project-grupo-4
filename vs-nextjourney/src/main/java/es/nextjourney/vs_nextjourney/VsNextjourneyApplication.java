2b18fe186f35920cd8edf36f5ce74416d860ea11
hurg05 <h.rus.2023@alumnos.urjc.es>
2026-02-10 09:58:26 +0100

    Add Spring file

---
+++ vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/VsNextjourneyApplication.java
@@ -0,0 +1,13 @@
+package es.nextjourney.vs_nextjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsNextjourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsNextjourneyApplication.class, args);
+	}
+
+}
---
+++ vs-nextjourney/src/test/java/es/nextjourney/vs_nextjourney/VsNextjourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package es.nextjourney.vs_nextjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsNextjourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

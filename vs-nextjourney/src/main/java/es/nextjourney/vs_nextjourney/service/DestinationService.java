3c3ce48d22a47eec9ef4088668b9d84dbe8e7404
hurg05 <h.rus.2023@alumnos.urjc.es>
2026-03-10 09:44:30 +0100

    Fix code

--- vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/service/DestinationService.java
+++ vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/service/DestinationService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import es.nextjourney.vs_nextjourney.javaClass.Destination;
+import es.nextjourney.vs_nextjourney.model.Destination;
 import es.nextjourney.vs_nextjourney.repository.DestinationRepository;
 
 @Service
--- vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/service/PlaceService.java
+++ vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/service/PlaceService.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import es.nextjourney.vs_nextjourney.javaClass.Place;
+import es.nextjourney.vs_nextjourney.model.Place;
 import es.nextjourney.vs_nextjourney.repository.PlaceRepository;
 
 @Service

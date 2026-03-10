f6a7cf1097384306035f7550066bef6ac05a3bd5
davidgutierrezovi18 <d.gutierrezo.2023@alumnos.urjc.es>
2026-03-10 09:44:29 +0100

    Create Image Service

--- vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/service/ImageService.java
+++ vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/service/ImageService.java
@@ -1,5 +1,48 @@
 package es.nextjourney.vs_nextjourney.service;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.sql.rowset.serial.SerialBlob;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import es.nextjourney.vs_nextjourney.repository.ImageRepository;
+import es.nextjourney.vs_nextjourney.model.Image;
+
+@Service
 public class ImageService {
-    
+
+    @Autowired
+    private ImageRepository imageRepository;
+
+    public Image createImage(MultipartFile imageFile) throws IOException {
+
+        Image image = new Image();
+
+        try {
+            image.setImageFile(new SerialBlob(imageFile.getBytes()));
+        } catch (Exception e) {
+            throw new IOException("Failed to create image", e);
+        }
+
+        imageRepository.save(image);
+
+        return image;
+    }
+
+    public Resource getImageFile(long id) throws SQLException {
+
+        Image image = imageRepository.findById(id).orElseThrow();
+
+        if (image.getImageFile() != null) {
+            return new InputStreamResource(image.getImageFile().getBinaryStream());
+        } else {
+            throw new RuntimeException("Image file not found");
+        }
+    }
 }

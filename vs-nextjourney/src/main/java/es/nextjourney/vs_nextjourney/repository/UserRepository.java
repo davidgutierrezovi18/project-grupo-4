386d8fcd61a152e76ab0680985d793f1762e495d
nsanz2024 <n.sanz.2024@alumnos.urjc.es>
2026-03-13 12:16:03 +0100

    modify userRepository

--- vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/repository/UserRepository.java
+++ vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package es.nextjourney.vs_nextjourney.repository;
-
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import es.nextjourney.vs_nextjourney.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    //deleteById
+    //findById
+    //save
+    //findAll
+    Optional<User> findByUserName(String userName); 
+    
+    Optional<User> findByEmail(String email);  
+
 
 }

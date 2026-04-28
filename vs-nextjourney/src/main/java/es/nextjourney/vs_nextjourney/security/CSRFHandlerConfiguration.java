56e71d1bd4fbe4493650c1f5816f5974661721c2
Mluzcm <ml.charfole.2024@alumnos.urjc.es>
2026-04-28 10:13:59 +0200

    Fix some errors

--- vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/security/CSRFHandlerConfiguration.java
+++ vs-nextjourney/src/main/java/es/nextjourney/vs_nextjourney/security/CSRFHandlerConfiguration.java
@@ -1,7 +1,5 @@
 package es.nextjourney.vs_nextjourney.security;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -9,23 +7,31 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 @Configuration
 public class CSRFHandlerConfiguration implements WebMvcConfigurer {
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new CSRFHandlerInterceptor());
-    }
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new CSRFHandlerInterceptor());
+	}
 }
 
 class CSRFHandlerInterceptor implements HandlerInterceptor {
-    @Override
-    public void postHandle(final HttpServletRequest request, final HttpServletResponse response, 
-                          final Object handler, final ModelAndView modelAndView) throws Exception {
-        if (modelAndView != null) {
-            CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
-            if (token != null) {
-                modelAndView.addObject("token", token.getToken());
-            }
-        }
-    }
+
+	@Override
+	public void postHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler,
+			final ModelAndView modelAndView) throws Exception {
+
+		if (modelAndView != null) {
+
+			CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+			if (token != null) {
+				modelAndView.addObject("token", token.getToken());
+			}
+		}
+	}
 }
+
